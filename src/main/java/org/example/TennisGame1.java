01aae5be448759555fc668b2c6ee862a2500282d
jagandeep <nice.jagan@gmail.com>
2020-10-29 22:49:10 +0530

    refactored switch statement to create scoreName method

--- src/main/java/org/example/TennisGame1.java
+++ src/main/java/org/example/TennisGame1.java
@@ -1,4 +1,5 @@
 package org.example;
+
 public class TennisGame1 {
 
     private int player1Points = 0;
@@ -12,19 +13,16 @@ public class TennisGame1 {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName.equals(this.player1Name) )
+        if (playerName.equals(this.player1Name))
             player1Points += 1;
         else
             player2Points += 1;
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (player1Points == player2Points)
-        {
-            switch (player1Points)
-            {
+        String score ;
+        if (player1Points == player2Points) {
+            switch (player1Points) {
                 case 0:
                     score = "Love-All";
                     break;
@@ -42,38 +40,21 @@ public class TennisGame1 {
                     break;
 
             }
+            return score;
         }
-        else if (player1Points >=4 || player2Points >=4)
-        {
+        if (player1Points >= 4 || player2Points >= 4) {
             int minusResult = player1Points - player2Points;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
+            return score;
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = player1Points;
-                else { score+="-"; tempScore = player2Points;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
-        return score;
+        return nameScore(player1Points)+"-"+nameScore(player2Points);
+    }
+
+    private String nameScore( int tempScore) {
+        String[] scoreNames = {"Love", "Fifteen", "Thirty", "Forty", "Deuce"};
+        return scoreNames[tempScore];
     }
 }
